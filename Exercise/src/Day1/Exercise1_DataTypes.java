2a72cc952cc92a643132b7e73f10a1807a37ba0e
Seema1995 <seemapatilk4@gmail.com>
2022-01-10 18:21:16 +0530

    Day 1 exercise files

---
+++ Exercise/src/Day1/Exercise1_DataTypes.java
@@ -0,0 +1,23 @@
+package Day1;
+
+public class Exercise1_DataTypes {
+	public static void main(String[] args) {
+		byte byteValue = 127;
+		int intValue = 122;
+		long longValue = 9900113344L;
+		
+		int charValue = 'A';
+		char charValue2 ='s';
+		
+		char charValue3 = 98;
+		
+		System.out.println("Byte Value : "+byteValue);
+		System.out.println("int Value : "+intValue);
+		System.out.println("long Value : "+longValue);
+		System.out.println("char Value : "+charValue );
+		System.out.println("char Value 2 : "+charValue2 );
+		System.out.println("char Value 3 : "+charValue3);
+		
+		
+	}
+}
---
+++ Exercise/src/Day1/Exercise2.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello "+getName("Seema")+"! Welcome to Java");
+	}
+	
+	public static String getName(String name) {
+		return name;
+	}
+
+}
