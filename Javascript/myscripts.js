b474b5f45677eff34d59d4ca67694dce5503b0ff
Seema1995 <seemapatilk4@gmail.com>
2022-01-19 11:40:28 +0530

    day 6 javascript exercise

---
+++ Javascript/myscripts.js
@@ -0,0 +1,14 @@
+function validateForm(){
+ var form = document.simpleForm;
+    var uname = form.un;
+    var pwd = form.pwd;
+    if(uname.value.length < 1){
+         alert("Username cannot be empty");
+        return false;
+    }
+    if(pwd.value.length < 1){
+        alert("Password cannot be empty");
+        return false;
+    }
+    //return true;
+}
